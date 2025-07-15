f0713ac3687aa80aaa7450af1fb0e7ce1acd617f
Jitesh-9311 <jiteshguleriagna@gmail.com>
2025-07-15 17:40:16 +0530

    commit

---
+++ day17.js
@@ -0,0 +1,71 @@
+// const date = new date()
+// console.log(date)
+
+// data method
+    // getfullyear()
+    // getmonth()    with index 0-6
+    // getdate()
+    // getday()       with index 0-6
+
+
+    // console.log(date.getfullyear())
+
+    // time  
+
+    // gethours()           without indexing
+    // getminutes()
+    // getseconds()
+    // getmilliseconds()
+
+    //  let a=date.gethours()
+    //  console.log(a)
+           
+    //  scheduler  
+
+    // setInterval()
+    // clearInterval()
+    // setTimeout()
+    // clearTimeout()
+
+
+    // reguler expression  like we create a pass with the @ or Character. denoted with regex
+
+    // syntax : pattern/modifier
+    // i modifier: case insenstive
+                                    // const a="hello world"
+                                    // const regex =/hello/i
+                                    // console.log(regex.test(a))
+
+    
+    // g modifier : global/check in only one line    
+                                //  const a="apple,mango,grapes,apple"
+                                //     const regex =/apple/g
+                                //     console.log(a.match(regex))
+    // m modifier : check in multiple lines .
+
+
+    // carrot sign ^ check first word or letter.
+    // dollar sign $
+
+    // quantifier : 
+    // * 
+    // +
+    // ?
+    // square bracket []   we use (0-1),voiwel,(A-Z)
+    // curly bracket {}    a{3}    aaa    or if we pass 2 it will show false  if more then 3 it will show true
+    // D : non digit character ko match krta hai like a,b,c.
+    // d:  match the digit (0-9)
+    // W: !,?,.  
+    // w: digit (0-9)
+    // n:minimum 
+    // m:maximum  
+
+    // null is basically a object.
+    
+    //  let a =null
+    //  letb=undefined
+    //  console.log(typeof(a))
+    //  console.log(typeof(b))
+
+    
+      
